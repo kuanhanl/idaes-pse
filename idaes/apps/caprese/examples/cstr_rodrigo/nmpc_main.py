294bac4931621a156d6ee6ea9fd078f818a45fe0
kuanhanl_DH4200 <kuanhanl@andrew.cmu.edu>
2021-10-25 16:58:11 -0400

    break nmpc_main into function pieces

--- idaes/apps/caprese/examples/cstr_rodrigo/nmpc_main.py
+++ idaes/apps/caprese/examples/cstr_rodrigo/nmpc_main.py
@@ -20,7 +20,8 @@ from idaes.apps.caprese.util import apply_noise_with_bounds
 from pyomo.environ import SolverFactory, Reference
 from pyomo.dae.initialization import solve_consistent_initial_conditions
 import idaes.logger as idaeslog
-from idaes.apps.caprese.examples.cstr_rodrigo.cstr_rodrigo_model import make_model
+from idaes.apps.caprese.examples.cstr_rodrigo.cstr_rodrigo_model import (
+        make_model)
 from idaes.apps.caprese.data_manager import PlantDataManager
 from idaes.apps.caprese.data_manager import ControllerDataManager
 from idaes.apps.caprese.plotlibrary import (
@@ -28,7 +29,6 @@ from idaes.apps.caprese.plotlibrary import (
         plot_control_input)
 
 __author__ = "Kuan-Han Lin"
-
 
 
 # See if ipopt is available and set up solver
@@ -43,17 +43,11 @@ if SolverFactory('ipopt').available():
 else:
     solver = None
 
-def main():
+def setup_controller():
     m_controller = make_model(horizon=10, ntfe=5, ntcp=2, bounds=True)
     sample_time = 2.
     m_plant = make_model(horizon=sample_time, ntfe=2, ntcp=2, bounds = True)
     time_plant = m_plant.t
-
-    simulation_horizon = 20
-    n_samples_to_simulate = round(simulation_horizon/sample_time)
-
-    samples_to_simulate = [time_plant.first() + i*sample_time
-                           for i in range(1, n_samples_to_simulate)]
 
     # We must identify for the controller which variables are our
     # inputs and measurements.
@@ -80,22 +74,6 @@ def main():
     plant = nmpc.plant
     controller = nmpc.controller
 
-    p_t0 = nmpc.plant.time.first()
-    c_t0 = nmpc.controller.time.first()
-    p_ts = nmpc.plant.sample_points[1]
-    c_ts = nmpc.controller.sample_points[1]
-
-    #--------------------------------------------------------------------------
-    # Declare variables of interest for plotting.
-    # It's ok not declaring anything. The data manager will still save some
-    # important data.
-    states_of_interest = [Reference(nmpc.plant.mod.Ca[:]),
-                          Reference(nmpc.plant.mod.Tall[:, "T"])]
-
-    plant_data= PlantDataManager(plant, states_of_interest)
-    controller_data= ControllerDataManager(controller, states_of_interest)
-    #--------------------------------------------------------------------------
-
     solve_consistent_initial_conditions(plant, plant.time, solver)
     solve_consistent_initial_conditions(controller, controller.time, solver)
 
@@ -104,8 +82,6 @@ def main():
     setpoint = [(controller.mod.Ca[0], 0.018)]
     setpoint_weights = [(controller.mod.Ca[0], 1.)]
 
-    # nmpc.controller.add_setpoint_objective(setpoint, setpoint_weights)
-    # nmpc.controller.solve_setpoint(solver)
     nmpc.controller.add_single_time_optimization_objective(setpoint,
                                                            setpoint_weights)
     nmpc.controller.solve_single_time_optimization(solver,
@@ -125,45 +101,29 @@ def main():
 
     nmpc.controller.initialize_to_initial_conditions()
 
+    # Declare variables of interest for plotting.
+    # It's ok not declaring anything. The data manager will still save some
+    # important data.
+    states_of_interest = [Reference(nmpc.plant.mod.Ca[:]),
+                          Reference(nmpc.plant.mod.Tall[:, "T"])]
 
+    # Setup data managers to save data
+    plant_data = PlantDataManager(plant, states_of_interest)
+    controller_data = ControllerDataManager(controller, states_of_interest)
+
+    return nmpc, plant_data, controller_data
+
+def solve_first_control_NLP(nmpc, plant_data, controller_data):
     # Solve the first control problem
     nmpc.controller.vectors.input[...].unfix()
     nmpc.controller.vectors.input[:,0].fix()
     solver.solve(nmpc.controller, tee=True)
     controller_data.save_controller_data(iteration = 0)
 
-    #-------------------------------------------------------------------------
-    #noise for measurements
-    variance = [
-        (nmpc.controller.mod.Tall[0, "T"], 0.05),
-        (nmpc.controller.mod.Tall[0, "Tj"], 0.02),
-        (nmpc.controller.mod.Ca[0], 1.0E-5),
-        ]
-    nmpc.controller.set_variance(variance)
-    measurement_variance = [
-            v.variance for v in controller.MEASUREMENT_BLOCK[:].var
-            ]
-    measurement_noise_bounds = [
-            (var[c_t0].lb, var[c_t0].ub)
-            for var in controller.MEASUREMENT_BLOCK[:].var
-            ]
-
-    # noise for inputs
-    variance = [
-        (plant.mod.Tjinb[0], 0.01),
-        ]
-    nmpc.plant.set_variance(variance)
-    input_variance = [v.variance for v in plant.INPUT_BLOCK[:].var]
-    input_noise_bounds = [
-        (var[p_t0].lb, var[p_t0].ub) for var in plant.INPUT_BLOCK[:].var
-        ]
-
-    random.seed(246)
-    #-------------------------------------------------------------------------
-
     # Extract inputs from controller and inject them into plant
-    inputs = controller.generate_inputs_at_time(c_ts)
-    plant.inject_inputs(inputs)
+    c_ts = nmpc.controller.sample_points[1]
+    inputs = nmpc.controller.generate_inputs_at_time(c_ts)
+    nmpc.plant.inject_inputs(inputs)
 
     plant_data.save_initial_plant_data()
 
@@ -173,46 +133,116 @@ def main():
     solver.solve(nmpc.plant, tee = True)
     plant_data.save_plant_data(iteration = 0)
 
-    for i in range(1, n_samples_to_simulate +1):
+    return nmpc, plant_data, controller_data
+
+def setup_noise(nmpc):
+    p_t0 = nmpc.plant.time.first()
+    c_t0 = nmpc.controller.time.first()
+
+    #noise for measurements
+    variance = [
+        (nmpc.controller.mod.Tall[0, "T"], 0.05),
+        (nmpc.controller.mod.Tall[0, "Tj"], 0.02),
+        (nmpc.controller.mod.Ca[0], 1.0E-5),
+        ]
+    nmpc.controller.set_variance(variance)
+    measurement_variance = [
+            v.variance for v in nmpc.controller.MEASUREMENT_BLOCK[:].var
+            ]
+    measurement_noise_bounds = [
+            (var[c_t0].lb, var[c_t0].ub)
+            for var in nmpc.controller.MEASUREMENT_BLOCK[:].var
+            ]
+
+    # noise for inputs
+    variance = [
+        (nmpc.plant.mod.Tjinb[0], 0.01),
+        ]
+    nmpc.plant.set_variance(variance)
+    input_variance = [v.variance for v in nmpc.plant.INPUT_BLOCK[:].var]
+    input_noise_bounds = [
+        (var[p_t0].lb, var[p_t0].ub) for var in nmpc.plant.INPUT_BLOCK[:].var
+        ]
+
+    noise_info = {"measurement_variance": measurement_variance,
+                  "input_variance": input_variance,
+                  "measurement_noise_bounds": measurement_noise_bounds,
+                  "input_noise_bounds": input_noise_bounds,}
+
+    return noise_info
+
+def run_iterations(nmpc,
+                   plant_data,
+                   controller_data,
+                   iterations = 10,
+                   has_noise = True,
+                   plot_results = True):
+
+    p_t0 = nmpc.plant.time.first()
+    c_t0 = nmpc.controller.time.first()
+    p_ts = nmpc.plant.sample_points[1]
+    c_ts = nmpc.controller.sample_points[1]
+
+    if has_noise:
+        noise_info = setup_noise(nmpc)
+        random.seed(246)
+
+    for i in range(1, iterations +1):
         print('\nENTERING NMPC LOOP ITERATION %s\n' % i)
         measured = nmpc.plant.generate_measurements_at_time(p_ts)
         nmpc.plant.advance_one_sample()
         nmpc.plant.initialize_to_initial_conditions()
-        measured = apply_noise_with_bounds(
-                measured,
-                measurement_variance,
-                random.gauss,
-                measurement_noise_bounds,
-                )
+        if has_noise:
+            measured = apply_noise_with_bounds(
+                    measured,
+                    noise_info["measurement_variance"],
+                    random.gauss,
+                    noise_info["measurement_noise_bounds"],
+                    )
 
         nmpc.controller.advance_one_sample()
         nmpc.controller.load_initial_conditions(measured)
 
-        solver.solve(nmpc.controller, tee=True)
+        solver.solve(nmpc.controller, tee = True)
         controller_data.save_controller_data(iteration = i)
 
-        inputs = controller.generate_inputs_at_time(c_ts)
-        inputs = apply_noise_with_bounds(
-                inputs,
-                input_variance,
-                random.gauss,
-                input_noise_bounds,
-                )
-        plant.inject_inputs(inputs)
+        inputs = nmpc.controller.generate_inputs_at_time(c_ts)
+        if has_noise:
+            inputs = apply_noise_with_bounds(
+                    inputs,
+                    noise_info["input_variance"],
+                    random.gauss,
+                    noise_info["input_noise_bounds"],
+                    )
+        nmpc.plant.inject_inputs(inputs)
 
         nmpc.plant.initialize_by_solving_elements(solver)
         nmpc.plant.vectors.input[...].fix() #Fix the input to solve the plant
         solver.solve(nmpc.plant, tee = True)
         plant_data.save_plant_data(iteration = i)
 
-    plot_setpoint_tracking_results(states_of_interest,
-                                   plant_data.plant_df,
-                                   controller_data.setpoint_df)
+    if plot_results:
+        states_of_interest = [Reference(nmpc.plant.mod.Ca[:]),
+                              Reference(nmpc.plant.mod.Tall[:, "T"])]
+        plot_setpoint_tracking_results(states_of_interest,
+                                       plant_data.plant_df,
+                                       controller_data.setpoint_df)
 
-    inputs_to_plot = [Reference(nmpc.plant.mod.Tjinb[:])]
-    plot_control_input(inputs_to_plot, plant_data.plant_df)
+        inputs_to_plot = [Reference(nmpc.plant.mod.Tjinb[:])]
+        plot_control_input(inputs_to_plot, plant_data.plant_df)
 
     return nmpc, plant_data, controller_data
 
 if __name__ == '__main__':
-    nmpc, plant_data, controller_data = main()
+    nmpc, plant_data, controller_data = setup_controller()
+    nmpc, plant_data, controller_data = solve_first_control_NLP(
+                                    nmpc,
+                                    plant_data,
+                                    controller_data)
+    nmpc, plant_data, controller_data = run_iterations(
+                                    nmpc,
+                                    plant_data,
+                                    controller_data,
+                                    iterations = 10,
+                                    has_noise = True,
+                                    plot_results = True)
